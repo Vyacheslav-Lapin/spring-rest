7e7aa7be61aaa112a96d1329b10786f1d1a11de8
VLapin <Vyacheslav.Lapin@gmail.com>
2024-09-26 13:50:27 +0300

    delombok via build profile in maven

--- src/main/java/com/luxoft/springadvanced/springrest/exception/PassengerNotFoundException.java
+++ src/main/java/com/luxoft/springadvanced/springrest/exception/PassengerNotFoundException.java
@@ -2,9 +2,15 @@ package com.luxoft.springadvanced.springrest.exception;
 
 import lombok.experimental.StandardException;
 
+import java.io.Serial;
+
 @StandardException
 public class PassengerNotFoundException extends RuntimeException {
-    public PassengerNotFoundException(Long id) {
+
+  @Serial
+  private static final long serialVersionUID = 36386186441847579L;
+
+  public PassengerNotFoundException(Long id) {
         super("Passenger id not found : " + id);
     }
 }
